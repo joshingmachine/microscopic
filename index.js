19c7023217299fca0355c9f08c2bfeec75499fda
joshingmachine <josh@joshingmachine.com>
2016-09-02 00:08:23 -0400

    Adding dust and first bacterium

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
+var compression = require("compression");
 var express = require("express");
 var app = express();
+var path = require("path");
+
+//app.use(compression());
+app.use(express.static("./"));
 
 app.get("/", function (req, res) {
-    res.send('Hello World!');
+    res.sendFile(path.join(__dirname + "/index.html"));
 });
 
 app.use(function(req, res, next) {
-    res.status(404).send('Sorry cant find that!');
+    res.status(404).send("Sorry can't find that!");
 });
 
 app.use(function(err, req, res, next) {
     console.error(err.stack);
-    res.status(500).send('Something broke!');
+    res.status(500).send("Something broke!");
 });
 
 app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+    console.log("Example app listening on port 3000!");
 });
