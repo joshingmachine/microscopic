962f85c167f87fa9673b9cfe9bfa379c9e7c9757
Josh Smith <joshingmachine@users.noreply.github.com>
2016-09-30 11:13:02 -0400

    Added more tutor dialog

--- src/constants/microbes/dialog/tutor.js
+++ src/constants/microbes/dialog/tutor.js
@@ -7,13 +7,23 @@ module.exports = [
         "I lost my three friends...",
         "Can you help me find them?",
         "They must be somewhere around here."
-    ] , [
+    ], [
         "I need to find my friends.",
         "Can you help me?",
         "Their phones must be dead or something."
-    ] , [
+    ], [
         "Do you know where my friends are?",
         "I need them to help me move my couch into my new apartment.",
         "My place is on the fourth floor, no elevator..."
+    ], [
+        "Can you help me look for my friends?",
+        "They're somewhere in this petri dish..."
+    ], [
+        "Where are my friends?",
+        "Did the three of them go to Tanya's without me?",
+        "Why, Tanya, why?"
+    ], [
+        "I can't find my friends...",
+        "Can you help me look for them?"
     ]
 ];
