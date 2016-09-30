3ae0a71a0aee417f15092c0fb5ddc8c6863c5898
Josh Smith <joshingmachine@users.noreply.github.com>
2016-09-30 12:32:46 -0400

    Adding more dialog for strangers

--- src/constants/microbes/dialog/strangers.js
+++ src/constants/microbes/dialog/strangers.js
@@ -1,7 +1,31 @@
 module.exports = [
     [
-        "Blah"
+        "Midichlorians?",
+        "Nah, never heard of him."
     ], [
-        "Bloop"
+        "That muffin you dropped on the floor?",
+        "I only waited 4 seconds to contaminate it...",
+        "MWAH HA HA HA HA!"
+    ], [
+        "I met Osmosis Jones at a nightclub once.",
+        "He was such a jerk.",
+        "He almost started a fight with the bouncer."
+    ], [
+        "I met Osmosis Jones at a nightclub once.",
+        "He was the sweetest guy.",
+        "He bought a round of shots for our whole table."
+    ], [
+        "I want to go somewhere with more culture."
+    ], [
+        "I used to live in Las Pancreas, but everyone there is so vacuous.",
+    ], [
+        "I'm sorry I'm E. coli.",
+        "I wish I was someone else..."
+    ], [
+        "I'm so small, compared to you...",
+        "Be honest with me: Do you think I matter at all?",
+        "How am I supposed to make a difference compared to big people like you?"
+    ], [
+        "Some microbes here are so cellfish"
     ]
 ];
